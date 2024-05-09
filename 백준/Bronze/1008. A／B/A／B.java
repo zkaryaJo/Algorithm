7c7e9fa356a4630409ad40cac830207989c1fb75
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-05-09 22:27:55 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 17952 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		double a = scanner.nextDouble();
+		double b = scanner.nextDouble();
+		
+		System.out.println(a/b);
+		
+		
+	}
+}
