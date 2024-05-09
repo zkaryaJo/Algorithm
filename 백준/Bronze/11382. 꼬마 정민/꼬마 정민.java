719c5cb4ae92b6040ffb3c7d86c67a23d77079e1
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-05-09 22:35:05 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);    
+        long a = scan.nextLong();
+        long b = scan.nextLong();
+        long c = scan.nextLong();
+        long sum = a+b+c;
+        System.out.println(sum);
+    }
+}
