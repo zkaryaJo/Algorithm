ad20dee55402d44f69c844566dcfe21ecf0a00ed
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-05-09 22:34:29 +0900

    [Bronze V] Title: A+B - 3, Time: 244 ms, Memory: 17972 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		int count = scanner.nextInt();
+
+		for (int i = 0; i < count; i++) {
+			
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			
+			System.out.println(a+b);
+			
+		}
+
+		scanner.close();
+	}
+}
