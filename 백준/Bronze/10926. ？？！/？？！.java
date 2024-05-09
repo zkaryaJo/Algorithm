1a811f0c2e863832f2477e0a77fbbbd5972a44a9
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-05-09 22:34:22 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+    
+        System.out.println(input+"??!");    
+    }
+    
+}
