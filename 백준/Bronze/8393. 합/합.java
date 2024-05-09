0bc2be5d117d55e22f1af1e6eb65c7c0d486ea23
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-05-09 22:32:28 +0900

    [Bronze V] Title: 합, Time: 216 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int x = scanner.nextInt();
+		
+		System.out.println((x*(x+1)/2));
+		
+		scanner.close();
+	}
+}
