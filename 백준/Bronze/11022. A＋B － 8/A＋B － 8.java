3d6042785319f92ac14f8fb43203383d03e0df63
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-05-09 23:07:25 +0900

    [Bronze V] Title: A+B - 8, Time: 280 ms, Memory: 19008 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		int count = scanner.nextInt();
+
+		for (int i = 0; i < count; i++) {
+			
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			
+			System.out.println("Case #"+(i+1)+": "+a+" + "+b+" = "+(a+b));
+			
+		}
+
+		scanner.close();
+	}
+}
