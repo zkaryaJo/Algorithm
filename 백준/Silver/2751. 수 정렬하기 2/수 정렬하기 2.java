aebb94e4a99ef5170e0da088e7c3d12f77f7c909
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-05-18 18:10:40 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1604 ms, Memory: 127064 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args){
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            int count = Integer.parseInt(br.readLine());
+            int[] arr = new int[count];
+            
+            for(int i=0; i<count; i++){
+                arr[i] = Integer.parseInt(br.readLine());
+            }
+            
+            arr = Arrays.stream(arr).sorted().toArray();
+            for(int i=0; i<arr.length; i++){
+                bw.write(arr[i]+"\n");
+            }    
+            bw.flush();
+        }catch(Exception e){
+            
+        }
+        
+        
+    }
+}
