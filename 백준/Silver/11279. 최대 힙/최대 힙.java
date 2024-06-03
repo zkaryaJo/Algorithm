66edf55e405e21d657e14699caaf2e574e686306
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-06-03 10:55:14 +0900

    [Silver II] Title: 최대 힙, Time: 1076 ms, Memory: 32568 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        PriorityQueue<Integer> pq  = new PriorityQueue<>(Collections.reverseOrder());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int count = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<count; i++){
+            int input = Integer.parseInt(br.readLine());
+            if(input == 0){
+                if(pq.isEmpty()){
+                    System.out.println(0);
+                }else{
+                    System.out.println(pq.poll());
+                }
+            }else{
+                pq.offer(input);
+            }
+        }
+    }
+}
