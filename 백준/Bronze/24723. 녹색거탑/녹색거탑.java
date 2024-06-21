d6c3991f8f98f875089654588c5f27303c719fd3
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-06-21 17:26:40 +0900

    [Bronze IV] Title: 녹색거탑, Time: 204 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String[ ] args){
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        System.out.println((int)Math.pow(2,N));
+    }
+}
